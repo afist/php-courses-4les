74c0739eb08768159df41a8386335131aa9bb80e
afist <barahuda@ua.fm>
2018-04-16 12:16:53 +0300

    1.1.0

--- form-send.php
+++ form-send.php
@@ -1,57 +1,59 @@
 <?php
 
 
-if (!empty($_POST)){
-    $expression = test_input($_POST["expression"]);
+if (!empty($_POST)) {
+    $expression = testInput($_POST["expression"]);
 }
-function test_input($data) {
-    $data = trim($data); // удаляем пробелы с конца и начала строки
-    $data = stripslashes($data); // Удаляет экранирование символов
-    $data = htmlspecialchars($data); //Преобразует специальные символы в HTML-сущности
+function testInput($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
     return $data;
 }
 
-function nicePrint($str){
+function nicePrint($str)
+{
     echo "<pre>";
     print_r($str);
     echo "</pre>";
-
 }
 
 
-function removeSpase($str){
+function removeSpase($str)
+{
     $arr = str_split($str);
-    for ($i = 0; $i < count($arr); $i++){
-        if ($arr[$i] == " "){
-             array_splice($arr,$i,1);
+    for ($i = 0; $i < count($arr); $i++) {
+        if ($arr[$i] == " ") {
+             array_splice($arr, $i, 1);
              $i--;
         }
-
-
     }
     $arr = charDel($arr);
     return implode('', $arr);
 }
 
-function checkNumber($a){
+function checkNumber($a)
+{
     return (48<=$a && $a<=58) or (46 ==$a) ? true : false;
 };
 
-function checkOperation($a){
+function checkOperation($a)
+{
     return ($a == 47) or ($a == 45) or ($a == 43) or ($a == 42) ? true : false;
 };
 
-function checkSymbol($str){
+function checkSymbol($str)
+{
     $arr = str_split($str);
     $count = count($arr);
     $countOperation = 0;
-    for ($i=0; $i < $count ; $i++) {
+    for ($i=0; $i<$count; $i++) {
         $number = checkNumber(ord($arr[$i]));
         $operation = checkOperation(ord($arr[$i]));
         
         if ($operation) {
             $countOperation++;
-            
         }
         if ($number or $operation) {
             continue;
@@ -59,7 +61,6 @@ function checkSymbol($str){
             echo "Error invalid characters";
             return false;
         }
-
     }
     if ($countOperation ==0) {
         echo "Error invalid characters";
@@ -68,85 +69,38 @@ function checkSymbol($str){
 
     return true;
 }
-function doubleCheck($str){
+function doubleCheck($str)
+{
     $arr = str_split($str);
     $count = count($arr)-1;
-    for ($i=0; $i < $count; $i++) { 
+    for ($i=0; $i < $count; $i++) {
         // echo checkOperation(ord($arr[$i]));
         // echo checkOperation(ord($arr[$i+1]));
         if (checkOperation(ord($arr[$i]))) {
-            if ( checkOperation(ord($arr[$i+1])) ) {
-            echo "Error invalid characters";
-            return false;
+            if (checkOperation(ord($arr[$i+1]))) {
+                echo "Error invalid characters";
+                return false;
             }
         }
-        
     }
     return true;
 }
-function charDel($arr){
-        for ($i=0; $i < count($arr) ; $i++) { 
-            if ( checkOperation(ord($arr[0])) ) {
-                array_splice($arr,0,1);
-            };
+function charDel($arr)
+{
+    for ($i=0; $i<count($arr); $i++) {
+        if (checkOperation(ord($arr[0]))) {
+            array_splice($arr, 0, 1);
+        };
             $lastChar = count($arr)-1;
-            if ( checkOperation(ord($arr[$lastChar])) ) {
-                array_splice($arr,$lastChar,1);
-            };
-        }
+        if (checkOperation(ord($arr[$lastChar]))) {
+            array_splice($arr, $lastChar, 1);
+        };
+    }
         return $arr;
-
 }
 
-// function matOperation($str){
-//     $arr = str_split($str);
-//     $count = count($arr);
-//     for ($i=0; $i < $count ; $i++) {
-//         if (checkOperation(ord($arr[$i]))) {
-//             $str_end = explode($arr[$i],$str);
-//             $operation = $arr[$i];
-//             // var_dump($str_end);
-//             // var_dump($operation);
-
-//             return [$str_end, $operation];
-//         }
-//     }
-// }
-
-
-// function endOperation($arr){
-// switch ($arr[1][0]) {
-//     case '*':
-//         return (int)$arr[0][0]*(int)$arr[0][1];
-//         break;
-//     case '/':
-//         return (int)$arr[0][0]/(int)$arr[0][1];
-//         break;
-//     case '-':
-//         return (int)$arr[0][0]-(int)$arr[0][1];
-//         break;
-//     case '+':
-//         return (int)$arr[0][0]+(int)$arr[0][1];
-//         break;
-// }
-// }
-
-
 $expression = removeSpase($expression);
-if (checkSymbol($expression) and doubleCheck($expression) ) {
-// $c = matOperation($expression);
-// $b = endOperation($c);
-eval('$result='.$expression.';');
-echo $expression." = ".$result;
+if (checkSymbol($expression) and doubleCheck($expression)) {
+    eval('$result='.$expression.';');
+    echo $expression." = ".$result;
 };
-
-
-//if (!empty($_POST)){
-//
-//echo "Method POST <br> Имя: ".$user_name."<br>Пароль: ".$password."<br>";
-//
-//}
-
-
-
-?>
